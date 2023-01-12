e0104f73041c7acc8179b2f8863db92b65b8df85
sergivg4 <sergivg10@gmail.com>
2023-01-12 17:37:10 +0100

    Vue router + estructura de páginas y componentes

---
+++ resources/js/components/admin/home/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Welcome to the admin overview dashboard</h1>
+    <div>
+        <router-link to="/">Home page</router-link>
+    </div>
+</template>
---
+++ resources/js/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+//admin
+import homeAdminIndex from '../components/admin/home/index.vue';
+
+//pages
+import homePageIndex from '../components/pages/home/index.vue';
+import notFound from '../components/notFound.vue';
+
+const routes = [
+    //admin
+    {
+        path: '/admin/home',
+        component: homeAdminIndex
+    },
+    //Pages
+    {
+        path: '/',
+        component: homePageIndex
+    },
+    //NotFound
+    {
+        path: '/:pathMatch(.*)*',
+        component: notFound
+    }
+]
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
